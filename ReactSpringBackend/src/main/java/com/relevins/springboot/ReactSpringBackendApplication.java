ef2e8ebebfc475e38382021da451fff2100f987a
Pugalendi Arumugam <89121032+arumugampblr@users.noreply.github.com>
2021-11-26 23:11:43 +0530

    first

---
+++ ReactSpringBackend/src/main/java/com/relevins/springboot/ReactSpringBackendApplication.java
@@ -0,0 +1,15 @@
+package com.relevins.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactSpringBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactSpringBackendApplication.class, args);
+	}
+
+	
+
+}
---
+++ ReactSpringBackend/src/test/java/com/relevins/springboot/ReactSpringBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.relevins.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactSpringBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
